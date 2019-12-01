f11ff7e60f02a17717842e9f3c06d0c5b94ce086
Max Prettyjohns <heshoots9999@gmail.com>
2019-12-01 21:43:50 +0000

    add second part

--- src/day_1.clj
+++ src/day_1.clj
@@ -17,14 +17,36 @@
     (/ 3)
     Math/floor
     (- 2)
+    (max 0)
   )
+)
+
+(defn more-fuel
+  ([mass]
+    (more-fuel mass 0)
+  )
+  ([mass, total]
+  (if (<= mass 0)
+    total
+    (
+      recur (calculate-fuel mass) (+ total (calculate-fuel mass))
+    )
+  ))
 )
 
 (defn -main [file]
   (println
-    (->> file
+    "base fuel " (->> file
       read-masses
       (map calculate-fuel)
+      (reduce +)
+    )
+  )
+  (println
+    "Fuel adds weight :O "
+    (->> file
+      read-masses
+      (map more-fuel)
       (reduce +)
     )
   )
